99c0f8c0c89fb1a1a3028d78c32a2039242fe20d
DumbTrollface <79498212+DumbTrollface@users.noreply.github.com>
2026-06-27 19:31:20 +0200

    added waypoints editor
    the editor shows handles for each point so that each waypoint can be moved from the scene view.

---
+++ Editor/WaypointsEditor.cs
@@ -0,0 +1,48 @@
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace DumbTrollface.Waypoints
+{
+    [CustomEditor(typeof(Waypoints))]
+    public class WaypointsEditor : Editor
+    {
+        private SerializedProperty waypointsProp;
+
+        private void OnEnable()
+        {
+            waypointsProp = serializedObject.FindProperty("waypoints");
+        }
+
+        private void OnSceneGUI()
+        {
+            serializedObject.Update();
+            int count = waypointsProp.arraySize;
+            Waypoints waypoints = (Waypoints) target;
+
+            // Iterate over all waypoints
+            for (int i = 0; i < count; i++)
+            {
+                // Read the property and convert the position from local to world space
+                SerializedProperty pointProp = waypointsProp.GetArrayElementAtIndex(i);
+                Vector3 worldPos = waypoints.transform.TransformPoint(pointProp.vector3Value);
+
+                // Create a label to make it easier to see which point is which
+                Handles.Label(worldPos, $"P{i}");
+
+                EditorGUI.BeginChangeCheck();
+                // Create a handle that can move an individual waypoint in the scene view
+                Vector3 newPos = Handles.PositionHandle(worldPos, Quaternion.identity);
+
+                // If the handle has been manipulated, we write the new value back to the property and apply the changes to the object
+                if (EditorGUI.EndChangeCheck())
+                {
+                    Vector3 newLocal = waypoints.transform.InverseTransformPoint(newPos);
+                    pointProp.vector3Value = newLocal;
+
+                    serializedObject.ApplyModifiedProperties();
+                }
+            }
+        }
+    }
+}
