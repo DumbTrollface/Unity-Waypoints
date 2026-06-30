f37ad0df03ac093ec7f2548499038a39051d65fa
DumbTrollface <79498212+DumbTrollface@users.noreply.github.com>
2026-06-30 16:49:20 +0200

    comments

--- Runtime/Waypoints.cs
+++ Runtime/Waypoints.cs
@@ -6,10 +6,10 @@ namespace DumbTrollface.Waypoints
     public class Waypoints : MonoBehaviour
     {
         /// <summary>
-        /// Determines if the path is displayed as closed (line from the last to first waypoint) or open.
+        /// Determines if the path is considered as closed (connection from the last to first waypoint) or open.
         /// </summary>
         [SerializeField]
-        [Tooltip("Should the path is displayed as closed (line from the last to first waypoint) or open.")]
+        [Tooltip("Should the path be considered as closed (connection from the last to first waypoint) or open.")]
         private bool closed = false;
 
         /// <summary>
@@ -24,10 +24,33 @@ namespace DumbTrollface.Waypoints
             new Vector3(-2f, 0f,  2f)
         };
 
+        /// <summary>
+        /// Return a readonly List of the waypoints.
+        /// </summary>
         public IReadOnlyList<Vector3> WaypointsList => waypoints;
+
+        /// <summary>
+        /// Returns the number of waypoints in this path
+        /// </summary>
         public int Count => waypoints.Count;
+
+        /// <summary>
+        /// Returns the state of the closed setting
+        /// </summary>
         public bool Closed => closed;
+
+        /// <summary>
+        /// Returns the position of a waypoint in local space
+        /// </summary>
+        /// <param name="index">index of the waypoint</param>
+        /// <returns>position in local space</returns>
         public Vector3 GetLocalPoint(int index) => waypoints[index];
+
+        /// <summary>
+        /// Returns the position of a waypoint in world space
+        /// </summary>
+        /// <param name="index">index of the waypoint</param>
+        /// <returns>position in world space</returns>
         public Vector3 GetWorldPoint(int index) => transform.TransformPoint(waypoints[index]);
     }
 
